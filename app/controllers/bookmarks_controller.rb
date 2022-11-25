60dbbbba76ac7344a208ae1a2d4b2475c3d8758b
Jeremy Buronyi <jeremy.buronyi@gmail.com>
2022-11-25 12:01:54 +0000

    Finalized the view

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -2,13 +2,20 @@ class BookmarksController < ApplicationController
   before_action :set_bookmark, only: :destroy
 
   def new
+    @movies = Movie.all
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new
   end
 
   def create
+    @list = List.find(params[:list_id])
     @bookmark = Bookmark.new(safe_params)
-    @bookmark.save
-    redirect_to list_path(@bookmark.list)
+    @bookmark.list = @list
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   def destroy
@@ -23,6 +30,6 @@ class BookmarksController < ApplicationController
   end
 
   def safe_params
-    params.require(:bookmark).permit(:comment, :movie_id, :list_id)
+    params.require(:bookmark).permit(:comment, :movie_id)
   end
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -14,8 +14,11 @@ class ListsController < ApplicationController
 
   def create
     @list = List.new(safe_params)
-    @list.save
-    redirect_to list_path(@list)
+    if @bookmark.save
+      redirect_to list_path(@list)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
